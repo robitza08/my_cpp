aa140a1d5408d89bc06ad3926ddf4c89ba502270
robitza08 <roby.poteleanu@gmail.com>
2023-07-30 14:12:39 +0300

    data types

---
+++ basic_data_types/source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char c = 'a';
+	cout << "\n\nThe value of variable c (type char) is: " << c;
+	int i = 23;
+	cout << "\nThe value of variable i (type integer) is: " << i;
+	bool b = true;
+	cout << "\nThe value of variable b (type boolean) is: " << b;
+	double d = 123.45;
+	cout << "\nThe value of variable d (type double) is: " << d;
+	cout << "\n\n";
+}
