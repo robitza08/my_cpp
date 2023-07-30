b51676072a593a40c5683794759389c90d4ce863
robitza08 <roby.poteleanu@gmail.com>
2023-07-30 11:43:27 +0300

    initial commit

---
+++ basic_math_operations/source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=78;
+    int b=24; 
+    int c=a+b; 
+    int d=a-b;
+	int x=a*b; 
+    int y=a/b;
+    cout<<"Valoarea lui a este: " << a << '\n';
+    cout<<"Valoarea lui b este: " << b << '\n';
+    cout<<"Suma a+b este: " << c << '\n';
+    cout<<"Diferenta a-b este: " << d << '\n';
+    cout<<"Produsul a*b este: " << x << '\n';
+    cout<<"Catul a/b este: " << y << '\n';
+}
