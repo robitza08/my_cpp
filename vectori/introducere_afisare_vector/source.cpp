7bf84ea9070ca58e259116f00e772310a493c7ee
robitza08 <roby.poteleanu@gmail.com>
2023-07-31 11:49:13 +0300

    introducerea unui vector de la tastatura si afisarea lui

---
+++ vectori/introducere_afisare_vector/source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; 
+	cout << "Introduceti dimensiunea (numarul de elemente) vectorului apoi apasati enter: ";
+    cin >> n;
+	
+    int a;
+    vector<int> v;
+	
+    cout << "Introduceti fiecare element al vectorului si apasati enter dupa fiecare element introdus \n";
+	
+    for (int i=0; i < n; i++) {
+        cin >> a;
+        v.push_back(a);
+    }
+	
+	cout << "Vectorul introdus este: \n";
+	
+	cout << "[";
+    for (int j=0; j < v.size(); j++) {
+        cout << v[j] << " ";
+    }
+    cout << "]";
+	
+    return 0;
+}
