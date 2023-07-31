afe78cb82ae9d26cff27246e543f547b000a96a1
robitza08 <roby.poteleanu@gmail.com>
2023-07-31 11:41:18 +0300

    Formatare cod

--- ultima_cifra_a_sumei/source.cpp
+++ ultima_cifra_a_sumei/source.cpp
@@ -6,18 +6,18 @@ int main()
 {
     int a;
     int b;
-    cout<<"** Acest program face suma a doua numere introduse de la tastatura si afiseaza ultima cifra a sumei *\n\n";
-    cout<<"Scrieti doua numere intregi separate prin spatiu: ";
+    cout <<"** Acest program face suma a doua numere introduse de la tastatura si afiseaza ultima cifra a sumei *\n\n";
+    cout <<"Scrieti doua numere intregi separate prin spatiu: ";
 
-    cin>>a>>b;
+    cin >> a >> b;
 
     int c=a+b;
     int d=c%10;
     
-    cout<<"Valoarea lui a este:" << a << '\n';
-    cout<<"Valoarea lui b este:" << b << '\n';
-    cout<<"Suma numerelor este:" << c << '\n';
-    cout<<"Ultima cifra a sumei numerelor este:" << d << '\n';
+    cout << "Valoarea lui a este:" << a << '\n';
+    cout << "Valoarea lui b este:" << b << '\n';
+    cout << "Suma numerelor este:" << c << '\n';
+    cout << "Ultima cifra a sumei numerelor este:" << d << '\n';
     
     return 0;
 }
