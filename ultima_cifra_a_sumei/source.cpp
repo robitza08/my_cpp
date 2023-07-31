9ed613d4def788126b7d94f8f68cb81a5af5ce23
robitza08 <roby.poteleanu@gmail.com>
2023-07-31 11:38:06 +0300

    Ultima cifra a sumei

---
+++ ultima_cifra_a_sumei/source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"** Acest program face suma a doua numere introduse de la tastatura si afiseaza ultima cifra a sumei *\n\n";
+    cout<<"Scrieti doua numere intregi separate prin spatiu: ";
+
+    cin>>a>>b;
+
+    int c=a+b;
+    int d=c%10;
+    
+    cout<<"Valoarea lui a este:" << a << '\n';
+    cout<<"Valoarea lui b este:" << b << '\n';
+    cout<<"Suma numerelor este:" << c << '\n';
+    cout<<"Ultima cifra a sumei numerelor este:" << d << '\n';
+    
+    return 0;
+}
