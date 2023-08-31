6419bec4cb19d3285a1cf4d24780ddb5fbaa4fd4
robitza08 <roby.poteleanu@gmail.com>
2023-08-31 13:35:49 +0300

    Afisarea cifrelor unui numar.

---
+++ afisarea_cifrelor_unui_numar/source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n; 
+    cout << "Introduceti un numar natural de la tastatura: ";
+    cin >> n;
+    cout << "Cifrele numarului sunt: ";
+    while(n != 0) // cat timp n este nenul - mai are cifre
+    {
+        int r = n % 10; //determinam ultima cifra a lui n
+        
+        // n /= 10;    //  eliminam ultima cifra (trunchiem numarul)
+        n = n / 10;
+        if (n != 0) {
+           cout << r << ", "; // prelucram ultima cifra
+        } else {
+           cout << r; 
+        }
+        
+    }
+    return 0;
+}
